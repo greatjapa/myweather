67b8f45decfac1a32cb833e14d483bcf424d9283
leandrocm86 <leandrocm86@gmail.com>
2018-06-09 15:25:48 -0300

    Modelo inicial

--- app/src/main/java/myweather/myweather/MainActivity.java
+++ app/src/main/java/myweather/myweather/MainActivity.java
@@ -2,22 +2,13 @@ package myweather.myweather;
 
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import myweather.myweather.modelo.cidades.Cidade;
+import myweather.myweather.modelo.servicos.ServicoMetaweather;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,48 +17,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new GetTask(this).execute("https://www.metaweather.com/api/location/455825/");
+        try {
+            new GetTask(this).execute();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 
     public class GetTask extends AsyncTask<String, Void, String> {
 
         private MainActivity activity;
-        private OkHttpClient client;
+        private Cidade rio = new Cidade("Rio");
+        private ServicoMetaweather metaweather = new ServicoMetaweather();
 
         GetTask(MainActivity activity) {
-            this.client = new OkHttpClient();
             this.activity = activity;
         }
 
         @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         protected String doInBackground(String... params) {
-            Request request = new Request.Builder()
-                    .url(params[0])
-                    .build();
-
-            try (Response response = client.newCall(request).execute()) {
-                return response.body().string();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
+            metaweather.buscarDados(rio);
+            return "ok";
         }
 
         protected void onPostExecute(String info) {
-            try {
-                JSONObject reader = new JSONObject(info);
-                JSONArray array = reader.getJSONArray("consolidated_weather");
-                JSONObject first = array.getJSONObject(0);
-                Double temp = first.getDouble("the_temp");
+            TextView viewById = activity.findViewById(R.id.value01ID);
+            viewById.setText(rio.getNome());
 
-                TextView viewById = activity.findViewById(R.id.value04ID);
-                viewById.setText(temp.toString());
-
-                TextView view = activity.findViewById(R.id.value05ID);
-                view.setText(temp.toString());
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            viewById = activity.findViewById(R.id.value05ID);
+            viewById.setText(rio.getTemperaturaMedia().toString());
         }
     }
 }
